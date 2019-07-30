a5d54061f3fc80a1019933811ce741a9763a6f0c
Thiago Macedo Silvestre <thiagomsilvestre@gmail.com>
2019-07-30 19:19:38 -0300

    refactoring config

--- src/database/elasticsearch/index.js
+++ src/database/elasticsearch/index.js
@@ -2,12 +2,12 @@
 
 const Config = require('config')
 const { Client } = require('elasticsearch')
-const client = new Client({ host: Config.elastic.host })
+const client = new Client({ host: Config.elasticsearch.connectionString })
 
 module.exports = class Elastic {
     static save(value) {
         return new Promise((resolve, reject) => {
-            let payload = {index: Config.elastic.index, body: value}
+            let payload = {index: Config.elasticsearch.index, body: value}
             let response = client.index(payload);
             resolve(response)
         })
